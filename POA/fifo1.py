cc686fc2ae1b8c05698a1217d3d90e21bbd40650
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-12-14 14:50:22 +0530

    practicing :)

--- POA/fifo1.py
+++ POA/fifo1.py
@@ -1,21 +1,26 @@
+# LRU
+
 count_memory=int(input("Enter number of memory blocks : "))
 seq=list(map(int,input("Enter a sequence ").split(' ')))
 
-block=[None for _ in range (count_memory)]
+blocks=[None for _ in range(count_memory)]
+timestamp=[-1 for _ in range(count_memory)]
 
 hits=0
 faults=0
 head=0
 
 for i,j in enumerate(seq):
-    if j not in block:
-        
-        block[head]=j
-        head=(head+1)%count_memory
-        
+    if j not in blocks:
+        #page fault
+        minimum=min(timestamp)
+        index=timestamp.index(minimum)
+        blocks[index]=j
+        timestamp[index]=i
         faults+=1
     else:
+      
         hits+=1
-    print(j," :\t",block)
-    
-print(f"Hits : {hits} \n Faults: {faults}")
+    print(j,":\t",blocks)
+
+print(f"Hits : {hits} \n Faults : {faults}")
