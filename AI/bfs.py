116b398ccc18c8fca1020f71b751dd3be5f6a4f6
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-12-17 12:48:00 +0530

    bfs

---
+++ AI/bfs.py
@@ -0,0 +1,32 @@
+def bfs(graph, src,goal,visited):
+    visited.append(src)
+    queue.append(src)
+    not_visited = [i for i in graph.keys() if i not in visited]   
+    print(f"{visited}\t\t{not_visited}\t\tFalse") 
+    while queue:
+        m = queue.pop(0)     
+        for i in graph[m]:
+            if i not in visited:
+                visited.append(i)
+                queue.append(i)
+                not_visited = [i for i in graph.keys() if i not in visited]
+                if goal in visited:
+                    print(f"{visited}\t\t{not_visited}\t\tTrue")                
+                    exit()
+                else:
+                    print(f"{visited}\t\t{not_visited}\t\tFalse")
+
+graph={
+    'A':['B','C'],
+    'B':['D','E'],
+    'C':['F'],
+    'D':[],
+    'E':[],
+    'F':[]
+}
+
+queue = []
+visited=[]
+src = input("Enter source node: ")
+goal = input("Enter goal node: ")
+bfs(graph, src, goal,visited)
